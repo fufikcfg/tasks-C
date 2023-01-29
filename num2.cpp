4a82fc56d148b620e404b4ca82345c61866cac8f
fufikcfg <cool.isaak2016@yandex.ru>
2023-01-29 13:11:43 +0500

    add new tasks

---
+++ num2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int number;
+    cout << "Enter a number: ";  
+    cin >> number;
+    if (number % 2 == 10) { cout << "This number " + number << " is even"; }
+    else {
+        cout << "This number " + number << " is odd";
+    }
+    return 0;
+}
