871c0a123bef1391e24c16aba48bf7d36a405963
fufikcfg <cool.isaak2016@yandex.ru>
2023-02-03 18:31:06 +0500

    new tasks

---
+++ num3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int numbers[3];
+    cout << "Enter any three random numbers:\n";
+    for (size_t i = 0; i <= 2; i++)
+    {
+        cin >> numbers[i];
+    }
+    if (numbers[0] > numbers[1]) {
+        cout << "Самое большое число - " << numbers[0];
+    }
+    else if (numbers[1] > numbers[2]) {
+        cout << "Самое большое число - " << numbers[1];
+    }
+    else if (numbers[2] > numbers[0]) {
+        cout << "Самое большое число - " << numbers[2];
+    }
+    else {
+        cout << "Числа одинаковые";
+    }
+    return 0;
+}
---
+++ num4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    float a,b,result;
+    unsigned int switches;
+    setlocale(LC_ALL, "ru");
+    cout << "Число 1 (уменьшаемое): ";
+    cin >> a;
+    cout << "Число 2 (вычитаемое): ";
+    cin >> b;
+    cout << "1:Деление\n2:Умножение\n3:Сумма\n4:Вычитание\n>> ";
+    cin >> switches;
+    switch (switches)
+    {
+    case 1:
+        result = a / b;
+        cout << result;
+        break;
+    case 2:
+        result = a * b;
+        cout << result;
+        break;
+    case 3:
+        result = a + b;
+        cout << result;
+        break;
+    case 4:
+        result = a - b;
+        cout << result;
+        break;
+    default:
+        cout << "invalid value";
+        break;
+    }
+    return 0;
+}
